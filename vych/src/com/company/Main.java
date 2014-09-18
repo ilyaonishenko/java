7edcafedbec7e7440509a30c02f918dd0c9ba173
wopqw <nurfick@gmail.com>
2014-09-19 03:20:44 +0400

    text in the begining

--- vych/src/com/company/Main.java
+++ vych/src/com/company/Main.java
@@ -24,82 +24,198 @@ public class Main {
         ArrayList nouns = new ArrayList();
         String word = "";
         int count=0;
-        // слова, которые вводятся в консоль точно попадают в allArray;
+        // слова, которые вводятся в консоль точно попадают в allArray; ------------- сделано
+        // включительно до окончания двойных окончаний слова разделяются на ис и глаголы.-----------сделано
+        // нужно сделать исключения для коротких слов------------сделано
+        // можно бояться слов типа привет и ещё чего-нибудь. Они принимаются за глаголы
+        // добавить суффикс "л" с плюшками
+        // добавить разделения на род число лицо и так далее (звучит плохо)
+        // разбить на функции
         for (int i=0;i<allArray.length;i++)
         {
             count=0;
             for (int j=0;j<verbEND.length;j++)
             {
-                if (j==0 || j== 1)
-                {
-                    word = String.valueOf(allArray[i].charAt(allArray[i].length()-1));
-                    if (word == verbEND[j])
-                    {
-                        verbs.add(allArray[i]);
-                    }
-                    else
-                    {
-                        count++;
-                    }
-                }
-                if (j==3 || j==2)
-                {
-                    word = String.valueOf(allArray[i].charAt(allArray[i].length()-2)).concat(String.valueOf(allArray[i].charAt(allArray[i].length()-1)));
-                    if (word.equals(verbEND[j]))
-                    {
-                        verbs.add(allArray[i]);
-                    }
-                    else
-                    {
-                        count++;
-                    }
-                }
-                if (j==5 || j == 4)
-                {
-                    word = String.valueOf(allArray[i].charAt(allArray[i].length()-2)).concat(String.valueOf(allArray[i].charAt(allArray[i].length()-1)));
-                    if (word.equals(verbEND[j]))
-                    {
-                        verbs.add(allArray[i]);
-                    }
-                    else
-                    {
-                        count++;
-                    }
-                }
-                if (j==7 || j==6)
-                {
-                    word = String.valueOf(allArray[i].charAt(allArray[i].length()-2)).concat(String.valueOf(allArray[i].charAt(allArray[i].length()-1)));
-                    if (word.equals(verbEND[j]))
-                    {
-                        verbs.add(allArray[i]);
-                    }
-                    else
-                    {
-                        count++;
-                    }
-                }
-                if (j==8 || j ==9)
-                {
-                    word = String.valueOf(allArray[i].charAt(allArray[i].length()-2)).concat(String.valueOf(allArray[i].charAt(allArray[i].length()-1)));
-                    if (word.equals(verbEND[j]))
-                    {
-                        verbs.add(allArray[i]);
-                    }
-                    else
-                    {
-                        count++;
-                    }
-                }
-                if (count==10)
+                if (allArray[i].length()<2)
                 {
                     nouns.add(allArray[i]);
-                    count=0;
+                    break;
+                }
+                else if (allArray[i].length()<3)
+                {
+                    if (j == 0 || j == 1) {
+                        word = String.valueOf(allArray[i].charAt(allArray[i].length() - 1));
+                        if (word.equals(verbEND[j])) {
+                            verbs.add(allArray[i]);
+                            break;
+                        } else {
+                            count++;
+                        }
+                    }
+                    if (j == 3 || j == 2) {
+                        word = String.valueOf(allArray[i].charAt(allArray[i].length() - 2)).concat(String.valueOf(allArray[i].charAt(allArray[i].length() - 1)));
+                        if (word.equals(verbEND[j])) {
+                            verbs.add(allArray[i]);
+                            break;
+                        } else {
+                            count++;
+                        }
+                    }
+                    if (count == 4)
+                    {
+                        nouns.add(allArray[i]);
+                        break;
+                    }
+                }
+                else if (allArray[i].length()<4)
+                {
+                    if (j == 0 || j == 1) {
+                        word = String.valueOf(allArray[i].charAt(allArray[i].length() - 1));
+                        if (word.equals(verbEND[j])) {
+                            verbs.add(allArray[i]);
+                            break;
+                        } else {
+                            count++;
+                        }
+                    }
+                    if (j == 3 || j == 2) {
+                        word = String.valueOf(allArray[i].charAt(allArray[i].length() - 2)).concat(String.valueOf(allArray[i].charAt(allArray[i].length() - 1)));
+                        if (word.equals(verbEND[j])) {
+                            verbs.add(allArray[i]);
+                            break;
+                        } else {
+                            count++;
+                        }
+                    }
+                    if (j == 5 || j == 4) {
+                        word = String.valueOf(allArray[i].charAt(allArray[i].length() - 2)).concat(String.valueOf(allArray[i].charAt(allArray[i].length() - 1)));
+                        if (word.equals(verbEND[j])) {
+                            verbs.add(allArray[i]);
+                            break;
+                        } else {
+                            count++;
+                        }
+                    }
+                    if (j == 7 || j == 6) {
+                        word = String.valueOf(allArray[i].charAt(allArray[i].length() - 2)).concat(String.valueOf(allArray[i].charAt(allArray[i].length() - 1)));
+                        if (word.equals(verbEND[j])) {
+                            verbs.add(allArray[i]);
+                            break;
+                        } else {
+                            count++;
+                        }
+                    }
+                    if (j == 8 || j == 9) {
+                        word = String.valueOf(allArray[i].charAt(allArray[i].length() - 2)).concat(String.valueOf(allArray[i].charAt(allArray[i].length() - 1)));
+                        if (word.equals(verbEND[j])) {
+                            verbs.add(allArray[i]);
+                            break;
+                        } else {
+                            count++;
+                        }
+                    }
+                    if (j == 10) {
+                        word = String.valueOf(allArray[i].charAt(allArray[i].length() - 3)).concat(String.valueOf(allArray[i].charAt(allArray[i].length() - 2))).concat(String.valueOf(allArray[i].charAt(allArray[i].length() - 1)));
+                        if (word.equals(verbEND[j])) {
+                            verbs.add(allArray[i]);
+                            break;
+                        } else count++;
+                    }
+                    if (count ==11)
+                    {
+                        nouns.add(allArray[i]);
+                        break;
+                    }
+                }
+                else {
+                    if (j == 0 || j == 1) {
+                        word = String.valueOf(allArray[i].charAt(allArray[i].length() - 1));
+                        if (word.equals(verbEND[j])) {
+                            verbs.add(allArray[i]);
+                            break;
+                        } else {
+                            count++;
+                        }
+                    }
+                    if (j == 3 || j == 2) {
+                        word = String.valueOf(allArray[i].charAt(allArray[i].length() - 2)).concat(String.valueOf(allArray[i].charAt(allArray[i].length() - 1)));
+                        if (word.equals(verbEND[j])) {
+                            verbs.add(allArray[i]);
+                            break;
+                        } else {
+                            count++;
+                        }
+                    }
+                    if (j == 5 || j == 4) {
+                        word = String.valueOf(allArray[i].charAt(allArray[i].length() - 2)).concat(String.valueOf(allArray[i].charAt(allArray[i].length() - 1)));
+                        if (word.equals(verbEND[j])) {
+                            verbs.add(allArray[i]);
+                            break;
+                        } else {
+                            count++;
+                        }
+                    }
+                    if (j == 7 || j == 6) {
+                        word = String.valueOf(allArray[i].charAt(allArray[i].length() - 2)).concat(String.valueOf(allArray[i].charAt(allArray[i].length() - 1)));
+                        if (word.equals(verbEND[j])) {
+                            verbs.add(allArray[i]);
+                            break;
+                        } else {
+                            count++;
+                        }
+                    }
+                    if (j == 8 || j == 9) {
+                        word = String.valueOf(allArray[i].charAt(allArray[i].length() - 2)).concat(String.valueOf(allArray[i].charAt(allArray[i].length() - 1)));
+                        if (word.equals(verbEND[j])) {
+                            verbs.add(allArray[i]);
+                            break;
+                        } else {
+                            count++;
+                        }
+                    }
+                    if (j == 10 || j == 11) {
+                        word = String.valueOf(allArray[i].charAt(allArray[i].length() - 3)).concat(String.valueOf(allArray[i].charAt(allArray[i].length() - 2))).concat(String.valueOf(allArray[i].charAt(allArray[i].length() - 1)));
+                        if (word.equals(verbEND[j])) {
+                            verbs.add(allArray[i]);
+                            break;
+                        } else count++;
+                    }
+                    if (j == 12 || j == 13) {
+                        word = String.valueOf(allArray[i].charAt(allArray[i].length() - 3)).concat(String.valueOf(allArray[i].charAt(allArray[i].length() - 2))).concat(String.valueOf(allArray[i].charAt(allArray[i].length() - 1)));
+                        if (word.equals(verbEND[j])) {
+                            verbs.add(allArray[i]);
+                            break;
+                        } else count++;
+                    }
+                    if (j == 14 || j == 15) {
+                        word = String.valueOf(allArray[i].charAt(allArray[i].length() - 3)).concat(String.valueOf(allArray[i].charAt(allArray[i].length() - 2))).concat(String.valueOf(allArray[i].charAt(allArray[i].length() - 1)));
+                        if (word.equals(verbEND[j])) {
+                            verbs.add(allArray[i]);
+                            break;
+                        } else count++;
+                    }
+                    if (j == 16 || j == 17) {
+                        word = String.valueOf(allArray[i].charAt(allArray[i].length() - 3)).concat(String.valueOf(allArray[i].charAt(allArray[i].length() - 2))).concat(String.valueOf(allArray[i].charAt(allArray[i].length() - 1)));
+                        if (word.equals(verbEND[j])) {
+                            verbs.add(allArray[i]);
+                            break;
+                        }
+                    }
+                    if (count == 16) {
+                        nouns.add(allArray[i]);
+                        count = 0;
+                        break;
+                    }
                 }
             }
         }
         for (int i=0;i<verbs.size();i++)
         {
-            System.out.println(verbs.get(i));
+            System.out.println("verbs: "+ verbs.get(i));
+        }
+        for (int i=0;i<nouns.size();i++)
+        {
+            System.out.println("nouns: "+nouns.get(i));
         }
     }
 }
