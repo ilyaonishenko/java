7e7ea647b12be5dec4f4b2a2d406eb5dcb46fe8c
wopqw <wopqw@yandex.ru>
2015-02-21 21:11:09 +0300

    some checks

--- complexConsole/check.java
+++ complexConsole/check.java
@@ -2,25 +2,101 @@ public class check
 {
   public void check(String text)
   {
-    twoStackAlgo tsa = new twoStackAlgo();
-    complexTwoStackAlgo ctsa = new complexTwoStackAlgo();
-    double answ = 0;
-    Complex cansw1 = new Complex(0,0);
-    boolean check = false;
+    boolean go = check2(text);
+    if (go==true)
+    {
+      twoStackAlgo tsa = new twoStackAlgo();
+      complexTwoStackAlgo ctsa = new complexTwoStackAlgo();
+      double answ = 0;
+      Complex cansw1 = new Complex(0,0);
+      boolean check = false;
+      for (int i=0;i<text.length();i++)
+      {
+        if (text.charAt(i)=='i')
+          check = true;
+      }
+      if (check == false)
+      {
+        answ = tsa.evaluation(text);
+        System.out.println(answ);
+      }
+      else if (check == true)
+      {
+        cansw1 =ctsa.complexEvaluation(text);
+        System.out.println(cansw1);
+      }
+    }
+    else
+    {
+      System.out.println("Bad");
+    }
+  }
+  public boolean check2(String text)
+  {
+    System.out.println("in check2");
+    boolean bracket = false;
+    boolean answer = true;
     for (int i=0;i<text.length();i++)
+      if (text.charAt(i)=='('||text.charAt(i)==')')
+        {
+          System.out.println("bracket = true");
+          bracket = true;
+          break;
+        }
+    if (text.length()==1||text.length()==0)
     {
-      if (text.charAt(i)=='i')
-        check = true;
+      System.out.println("length 1 or 0");
+      answer = false;
+      return answer;
     }
-    if (check == false)
+    else if (bracket == true)
     {
-      answ = tsa.evaluation(text);
-      System.out.println(answ);
+      System.out.println("bracket == true");
+      int count=0;
+      for (int i=0;i<text.length();i++)
+      {
+        if (text.charAt(i)=='(')
+          count++;
+        if (text.charAt(i)==')')
+          count++;
+      }
+      if (count==2)
+        answer = true;
+      else
+        answer = false;
     }
-    else if (check == true)
+    if (answer == true)
     {
-      cansw1 =ctsa.complexEvaluation(text);
-      System.out.println(cansw1);
+
+      parser p = new parser();
+      String[] array = p.parser(text);
+      String[] sep = new String[]{")","+","-","*","/"};
+      int countOps = 0;
+      int countVal = 0;
+      for (int i=0;i<array.length;i++)
+      {
+        for (int j=0;j<sep.length;j++)
+        {
+          if (array[i].equals(sep[j]))
+          {
+            countOps++;
+            break;
+          }
+        }
+      }
+      countVal = array.length-countOps;
+      if (countVal==countOps+1)
+      {
+        answer = true;
+        return answer;
+      }
+      else
+      {
+        System.out.println("Ops and Vals");
+        answer = false;
+        return answer;
+      }
     }
+    return answer;
   }
 }
