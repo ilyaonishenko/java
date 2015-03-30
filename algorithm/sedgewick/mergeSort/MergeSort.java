4411d234495a9c794d128f5cd575b978a97fc62d
wopqw <wopqw@yandex.ru>
2015-03-30 12:58:06 +0300

    checked mergeSort top down realisation

--- algorithm/sedgewick/mergeSort/MergeSort.java
+++ algorithm/sedgewick/mergeSort/MergeSort.java
@@ -1,10 +1,20 @@
+import java.util.Random;
 public class MergeSort{
 	private static int[] array;
+	public static void main(String[] args){
+		int[] array = new int[8];
+		Random r = new Random();
+		for(int i=0;i<array.length;i++)
+			array[i] = r.nextInt(10);
+		sort(array);
+		for(int i:array)
+			System.out.print(i+" ");
+	}
 	public static void sort(int[] arr){
 		array = new int[arr.length];
 		sort(arr,0,arr.length-1);
 	}
-	private static void sort(int[] array,int lo,int hi){
+	private static void sort(int[] a,int lo,int hi){
 		if (hi<=lo)
 			return;
 		int mid = lo+(hi-lo)/2;
@@ -15,6 +25,7 @@ public class MergeSort{
 	public static void merge(int[] a,int lo,int mid,int hi){
 		int i = lo;
 		int j = mid+1;
+		int[] aux = new int[a.length];
 		for(int k=lo;k<=hi;k++)
 			aux[k]=a[k];
 		for(int k=lo;k<=hi;k++){
@@ -23,12 +34,11 @@ public class MergeSort{
 			else if (j>hi)
 				a[k]=aux[i++];
 			else if (less(aux[j],aux[i]))
-				a[k]=aux[j++]
+				a[k]=aux[j++];
 			else a[k]=aux[i++];
 		}
 	}
 	public static boolean less(int v,int w){
 		return v<w;
 	}
-
 }
