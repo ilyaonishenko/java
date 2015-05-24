f52adccf0ede88cec1e77ba1788b08a17103a02e
wopqw <wopqw@yandex.ru>
2015-05-24 22:56:10 +0300

    pretty working i hope

--- RedditSender/src/main/java/com/rabbitsender/client/Sender.java
+++ RedditSender/src/main/java/com/rabbitsender/client/Sender.java
@@ -7,6 +7,8 @@ import javax.annotation.Resource;
 import java.io.IOException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by woqpw on 23.05.15.
@@ -16,69 +18,115 @@ public class Sender {
 //    RabbitAdmin rabbitAdmin;
     private static final String QUEUE_NAME = "Hello";
     private static final String BACKQUEUE_NAME = "BackHello";
-    private static String relplyQueue;
-    private static QueueingConsumer queueingConsumer;
+//    private static String relplyQueue;
+//    private static QueueingConsumer queueingConsumer;
+    private static Connection connection;
     private static Channel channel;
-    static final ExecutorService threadPool;
-    static {
-        threadPool = Executors.newCachedThreadPool();
-    }
     public static String answer;
     public static void main(String[] args){
         final ConnectionFactory connectionFactory = new ConnectionFactory();
         connectionFactory.setHost("localhost");
         try {
-            Connection connection = connectionFactory.newConnection();
+            connection = connectionFactory.newConnection();
             channel = connection.createChannel();
-            relplyQueue = channel.queueDeclare().getQueue();
-            queueingConsumer = new QueueingConsumer(channel);
-            channel.basicConsume(relplyQueue,true,queueingConsumer);
-            /*channel.queueDeclare(QUEUE_NAME,false,false,false,null);
+//            relplyQueue = channel.queueDeclare().getQueue();
+//            queueingConsumer = new QueueingConsumer(channel);
+//            channel.basicConsume(relplyQueue,true,queueingConsumer);
+            channel.queueDeclare(QUEUE_NAME,false,false,false,null);
             String message = "Hello world!";
             channel.basicPublish("",QUEUE_NAME,null,message.getBytes());
             System.out.println(" [x] Sent '" + message + "'");
-            /*channel.close();
+            channel.close();
             connection.close();
 //            String answer = receive(connectionFactory);
 //            System.out.println(answer + " answer is");
-            /*Thread newThread = new Thread(()->{
-                System.out.println("new thread");
+//            Thread newThread = new Thread(()->{
+//                System.out.println("new thread");
+//                answer = receive(connectionFactory);
+//                System.out.println("answer is "+answer);
+//            });
+            /*ScheduledExecutorService exec = Executors.newSingleThreadScheduledExecutor();
+            exec.scheduleAtFixedRate(new Runnable() {
+                @Override
+                public void run() {
+                    answer = receive(connectionFactory);
+                    System.out.println("answer is "+answer);
+                }
+            }, 0, 1, TimeUnit.SECONDS);*/
+
+
+            /*while(true){
                 answer = receive(connectionFactory);
-                System.out.println("answer is "+answer);
-            });
-            newThread.start();*/
+                System.out.println("i get "+answer);
+            }*/
+
+                connection = connectionFactory.newConnection();
+                channel = connection.createChannel();
+                channel.queueDeclare(BACKQUEUE_NAME,false,false,false,null);
+                QueueingConsumer queueingConsumer = new QueueingConsumer(channel);
+                channel.basicConsume(BACKQUEUE_NAME,true,queueingConsumer);
+            /*while (true){
+                QueueingConsumer.Delivery delivery = queueingConsumer.nextDelivery();
+                String message2 = new String(delivery.getBody());
+                System.out.println("get message2 "+message2);
+            }*/
+            ScheduledExecutorService exec = Executors.newSingleThreadScheduledExecutor();
+            exec.scheduleAtFixedRate(new Runnable() {
+                @Override
+                public void run() {
+                    answer = receive(connection,channel,queueingConsumer);
+                    System.out.println("answer is "+answer);
+                }
+            }, 0, 1, TimeUnit.SECONDS);
+            System.out.println("Hello world new time");
+//            System.out.println("Hello");
 //            while(true) {
-                answer = call("hello world");
-                System.out.println(answer);
+//                answer = call("hello world");
+//                System.out.println(answer);
+//            int status = channel.queueDeclarePassive(relplyQueue).getMessageCount();
+//            System.out.println("it is "+status+" messages in queue");
 //            }
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
-    /*public static String receive(ConnectionFactory connectionFactory){
+    public static String receive(Connection connection,Channel channel,QueueingConsumer queueingConsumer){
+        String answer;
+        while(true){
+            try {
+                QueueingConsumer.Delivery delivery = queueingConsumer.nextDelivery();
+                answer = new String(delivery.getBody());
+                return answer;
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+        }
+    }
+    public static String receive(ConnectionFactory connectionFactory){
         String answer;
         try {
-            Connection connection = connectionFactory.newConnection();
-            Channel channel = connection.createChannel();
+            connection = connectionFactory.newConnection();
+            channel = connection.createChannel();
             channel.queueDeclare(BACKQUEUE_NAME,false,false,false,null);
             QueueingConsumer queueingConsumer = new QueueingConsumer(channel);
             channel.basicConsume(BACKQUEUE_NAME,true,queueingConsumer);
-            while(true){
+//            while(true){
+            int status = channel.queueDeclarePassive(BACKQUEUE_NAME).getMessageCount();
+            System.out.println("it is "+status+" messages in queue");
                 QueueingConsumer.Delivery delivery = queueingConsumer.nextDelivery();
                 answer = new String(delivery.getBody());
                 System.out.println("Received "+answer);
-                int status = channel.queueDeclarePassive(BACKQUEUE_NAME).getMessageCount();
-                System.out.println("it is "+status+" messages in queue");
                 return answer;
-            }
+//            }
         } catch (IOException e) {
             e.printStackTrace();
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
         return null;
-    }*/
-    public static String call(String message){
+    }
+    /*public static String call(String message){
         String response = null;
         String corrId = java.util.UUID.randomUUID().toString();
         AMQP.BasicProperties basicProperties = new AMQP.BasicProperties
@@ -92,6 +140,7 @@ public class Sender {
                 QueueingConsumer.Delivery delivery = queueingConsumer.nextDelivery();
                 if(delivery.getProperties().getCorrelationId().equals(corrId)){
                     response = new String(delivery.getBody());
+                    response += new String(delivery.getBody());
                     return response;
                 }
 
@@ -103,5 +152,5 @@ public class Sender {
             e.printStackTrace();
         }
         return null;
-    }
+    }*/
 }
--- RedditSender/src/main/java/com/rabbitsender/server/Server.java
+++ RedditSender/src/main/java/com/rabbitsender/server/Server.java
@@ -18,49 +18,52 @@ public class Server {
             Connection connection = connectionFactory.newConnection();
             Channel channel = connection.createChannel();
             channel.queueDeclare(QUEUE_NAME,false,false,false,null);
-            channel.basicQos(1);
+//            channel.basicQos(1);
             System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
             QueueingConsumer queueingConsumer = new QueueingConsumer(channel);
-            channel.basicConsume(QUEUE_NAME,false,queueingConsumer);
+            channel.basicConsume(QUEUE_NAME,true,queueingConsumer);
             System.out.println(" [x] Awaiting RPC requests");
             while (true){
                 QueueingConsumer.Delivery delivery = queueingConsumer.nextDelivery();
-                BasicProperties basicProperties = delivery.getProperties();
+//                BasicProperties basicProperties = delivery.getProperties();
 //                AMQP.BasicProperties basicProperties = delivery.getProperties();
-                AMQP.BasicProperties basicProperties1 = new AMQP.BasicProperties
-                        .Builder()
-                        .correlationId(basicProperties.getCorrelationId())
-                        .build();
+//                AMQP.BasicProperties basicProperties1 = new AMQP.BasicProperties
+//                        .Builder()
+//                        .correlationId(basicProperties.getCorrelationId())
+//                        .build();
                 String message = new String(delivery.getBody());
                 System.out.println(" [x] Received '" + message + "'");
-//                Thread.sleep(5000);
-                String response  = "some message from server for "+message;
-                System.out.println(response);
-                String[] responseArray = response.split(" ");
-                for (String s:responseArray){
-                    channel.basicPublish("", basicProperties.getReplyTo(), basicProperties1, s.getBytes());
-                }
-                channel.basicAck(delivery.getEnvelope().getDeliveryTag(),false);
-                /*System.out.println("Preparing to send message back");
+                Thread.sleep(5000);
+//                String response  = "some message from server for "+message;
+//                System.out.println(response);
+//                String[] responseArray = response.split(" ");
+//                for (String s:responseArray){
+//                    channel.basicPublish("", basicProperties.getReplyTo(), basicProperties1, s.getBytes());
+//                    System.out.println(s+" is sended");
+////                    int status = channel.queueDeclarePassive(basicProperties.getReplyTo()).getMessageCount();
+////                    System.out.println("it is "+status+" messages in queue");
+//                }
+//                channel.basicAck(delivery.getEnvelope().getDeliveryTag(),false);
+                System.out.println("Preparing to send message back");
                 Connection connection1 = connectionFactory.newConnection();
                 System.out.println("Connection ok");
                 Channel channel1 = connection1.createChannel();
                 System.out.println("Channel ok");
                 channel1.queueDeclare(BACKQUEUE_NAME,false,false,false,null);
                 System.out.println("Queue declared");
-                message = "helloFormServer "+message;
+                message = "helloFormServer and the last message: "+message;
                 int status;
                 String[] array = message.split(" ");
                 for (String a:array){
                     channel1.basicPublish("",BACKQUEUE_NAME, null, a.getBytes());
                     Thread.sleep(2000);
                     System.out.println(a + " is sended");
-                    status = channel1.queueDeclarePassive(BACKQUEUE_NAME).getMessageCount();
-                    System.out.println("it is "+status+" messages in queue");
+//                    status = channel1.queueDeclarePassive(BACKQUEUE_NAME).getMessageCount();
+//                    System.out.println("it is "+status+" messages in queue");
                 }
                 System.out.println(" all ok");
                 channel1.close();
-                connection1.close();*/
+                connection1.close();
 
             }
         } catch (IOException e) {
